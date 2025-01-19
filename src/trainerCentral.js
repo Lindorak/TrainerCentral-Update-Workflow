71547a564de35243d0ae09522f34e329c96d9fc9
Lindorak <Lindorak@gmail.com>
2025-01-19 01:30:52 -0500

    Update trainerCentral.js

--- src/trainerCentral.js
+++ src/trainerCentral.js
@@ -1,6 +1,7 @@
 import { TrainerCentralAPI } from './trainerCentralAPI.js';
 
 const tokenKey = 'trainercentral_token';
+const ACCOUNT_ID = globalThis.ACCOUNT_ID;
 
 export async function getOrSetCachedToken() {
   let token = await KV_TCUK.get(tokenKey);
@@ -13,12 +14,12 @@ export async function getOrSetCachedToken() {
 
 export async function fetchCourses() {
   const token = await getOrSetCachedToken();
-  const response = await TrainerCentralAPI.getCourses(token);
+  const response = await TrainerCentralAPI.getCourses(token, ACCOUNT_ID);
   return response.data;
 }
 
 export async function fetchLessons(courseId) {
   const token = await getOrSetCachedToken();
-  const response = await TrainerCentralAPI.getLessons(token, courseId);
+  const response = await TrainerCentralAPI.getLessons(token, courseId, ACCOUNT_ID);
   return response.data;
 }
