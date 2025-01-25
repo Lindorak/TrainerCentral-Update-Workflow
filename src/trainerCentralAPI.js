6f0a2072898e000b85ed64d6cc2b6a611b10549d
Lindorak <Lindorak@gmail.com>
2025-01-25 01:39:24 -0500

    Update trainerCentralAPI.js

--- src/trainerCentralAPI.js
+++ src/trainerCentralAPI.js
@@ -1,28 +1,30 @@
-const BASE_URL = 'https://pathway-to-hope.trainercentral.ca/api/v4';
+const BASE_URL = 'https://accounts.zohocloud.ca/';
 
 export const TrainerCentralAPI = {
   async authenticate() {
-    const response = await fetch(`${BASE_URL}/auth`, {
+    const response = await fetch(`${BASE_URL}/oauth/v2/token`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         clientId: CLIENT_ID,
         clientSecret: CLIENT_SECRET
+        granttype: client_credentials
+        scope: TrainerCentral.courseapi.ALL,TrainerCentral.sessionapi.ALL
       })
     });
     const data = await response.json();
     return data.token;
   },
 
-  async getCourses(token, accountId) {
-    const response = await fetch(`${BASE_URL}/accounts/${accountId}/courses`, {
+  async getCourses(token, orgId) {
+    const response = await fetch(`${BASE_URL}/${orgId}/courses`, {
       headers: { Authorization: `Bearer ${token}` }
     });
     return response.json();
   },
 
   async getLessons(token, courseId, accountId) {
-    const response = await fetch(`${BASE_URL}/accounts/${accountId}/courses/${courseId}/lessons`, {
+    const response = await fetch(`${BASE_URL}/${orgId}/courses/${courseId}/lessons`, {
       headers: { Authorization: `Bearer ${token}` }
     });
     return response.json();
